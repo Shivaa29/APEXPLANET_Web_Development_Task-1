eaf65ec03ae37c14a36234b7ffa7e90e38752825
Shivaa29 <132735589+Shivaa29@users.noreply.github.com>
2025-04-30 12:03:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('alertButton').addEventListener('click', function () {
+    alert('Hello! Thank you for visiting my portfolio.');
+});
+
+document.getElementById('linkedinButton').addEventListener('click', function () {
+    window.open('https://www.linkedin.com/in/shiva-prasad-reddy-siripuram', '_blank');
+});
